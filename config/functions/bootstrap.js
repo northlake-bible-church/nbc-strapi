3114286b667c363ebabcdc8854a92089d37724f1
Tobias <tobiasgunn@gmail.com>
2020-03-22 15:51:39 -0500

    Added Banner and Modal Singles

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,20 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+const socketInit = async () => {
+  // import socket io
+  var io = require("socket.io")(strapi.server);
+  console.log(strapi.server);
+  // listen for user connection
+  io.on("connection", function(socket) {
+    // send message on user connection
+    socket.emit("hello", JSON.stringify({ message: "Test sockets" }));
+    // listen for user diconnect
+    socket.on("disconnect", () => console.log("a user disconnected"));
+  });
+  strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
+};
+
+module.exports = () => {
+  return socketInit();
+};
